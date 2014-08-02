07b29c9850de10205f95442353f493a4d971a86d
Я Чайка <mellanie335@gmail.com>
2014-08-02 16:20:08 +0900

    Add exception handler

--- NicoLiveAlert/Application/NicoLiveAlert.m
+++ NicoLiveAlert/Application/NicoLiveAlert.m
@@ -8,15 +8,56 @@
 
 #import "NicoLiveAlert.h"
 
-@implementation NicoLiveAlert
+@interface NicoLiveAlert ()
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+#ifdef __cplusplus
+extern "C" {
+#endif
+	static void uncaughtExceptionHandler(NSException *exception);
+#ifdef __cplusplus
+} //end extern "C"
+#endif
+@end
+
+@implementation NicoLiveAlert
+#pragma mark - synthesize properties
+#pragma mark - class method
+#pragma mark - constructor / destructor
+#pragma mark - override
+- (void) awakeFromNib
 {
-	// Insert code here to initialize your application
-}
+	NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
+	
+}// end - (void) awakeFromNib
+#pragma mark - delegate
+- (void) applicationWillFinishLaunching:(NSNotification *)notification
+{
+	
+}// end - (void) applicationWillFinishLaunching:(NSNotification *)notification
+
+- (void) applicationDidFinishLaunching:(NSNotification *)notification
+{
+		// Insert code here to initialize your application
+}// end - (void) applicationDidFinishLaunching:(NSNotification *)notification
+
+#pragma mark - instance method
+#pragma mark - properties
+#pragma mark - actions
+#pragma mark - messages
+#pragma mark - private
+#pragma mark - C functions
 
 - (IBAction) openPreference:(id)sender
 {
 	NSLog(@"open message recieved %@", sender);
 }// end - (IBAction) openPreference:(id)sender
+static
+
+void uncaughtExceptionHandler(NSException *exception)
+{
+	NSLog(@"Exception Name %@, Reason %@", exception.name, exception.reason);
+	NSLog(@"Exception Description %@", exception.description);
+	NSLog(@"Exception Call Stack Symbols %@", exception.callStackSymbols);
+}// end void uncaughtExceptionHandler(NSException *exception)
+
 @end
