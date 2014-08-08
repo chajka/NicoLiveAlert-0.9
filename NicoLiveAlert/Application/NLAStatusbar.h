fae3a8029a072e3cc6798a23502624c0af57bf2b
Я Чайка <mellanie335@gmail.com>
2014-08-08 18:49:44 +0900

    Add empty class NLAStatusbar

---
+++ NicoLiveAlert/Application/NLAStatusbar.h
@@ -0,0 +1,13 @@
+//
+//  NLAStatusbar.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 8/8/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLAStatusbar : NSObject
+
+@end
---
+++ NicoLiveAlert/Application/NLAStatusbar.m
@@ -0,0 +1,13 @@
+//
+//  NLAStatusbar.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 8/8/14.
+//  Copyright (c) 2014 Instrumentality of mankind. All rights reserved.
+//
+
+#import "NLAStatusbar.h"
+
+@implementation NLAStatusbar
+
+@end
